2013377833c9038f7df8651f1c4f7e8c805a400f
Keven Mendes <dgkeven@outlook.com>
2024-04-21 12:24:55 -0300

    Add files via upload

---
+++ PraticaIfElse.java
@@ -0,0 +1,17 @@
+package academy.meuprimeiroprograma.maratonajava.introducao;
+
+public class PraticaIfElse {
+    public static void main(String[] args) {
+        int salarioAtual = 2300;
+        double pretencaoSalarial = 2800;
+        double salarioPromessa = 2500;
+        if (salarioPromessa >= pretencaoSalarial) {
+            System.out.println("Continuarei na empresa!");
+        } else {
+            if (pretencaoSalarial <= salarioPromessa) ;
+            System.out.println("Obrigado mas buscarei uma oportunidade melhor!");
+        }
+    }
+}
+
+
