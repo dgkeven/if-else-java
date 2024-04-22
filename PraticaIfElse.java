760bc9b6e9993b2c5f2c5af37c7fee45b18936cf
Keven Mendes <dgkeven@outlook.com>
2024-04-22 07:10:57 -0300

    Add files via upload

---
+++ PraticaIfElse.java
@@ -0,0 +1,17 @@
+package academy.meuprimeiroprograma.maratonajava.introducao;
+
+public class PraticaIfElse {
+    public static void main(String[] args) {
+        int valorAtual = 12000;
+        double pcNovo = 17000;
+        double pcUsado = 10000 ;
+        if (valorAtual >= pcNovo) {
+            System.out.println("Comparei um pc novo!!");
+        } else {
+            if (valorAtual <= pcUsado) ;
+            System.out.println("Comprarei um pc usado!");
+        }
+    }
+}
+
+
