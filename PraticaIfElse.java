18aad924f61cea52faee8a18de6cad7a0db4e03f
Keven Mendes <dgkeven@outlook.com>
2024-11-28 20:18:45 -0300

    PraticaIfElse.java

--- PraticaIfElse.java
+++ PraticaIfElse.java
@@ -1,16 +1,15 @@
-
 public class PraticaIfElse {
     public static void main(String[] args) {
         int valorAtual = 12000;
         double pcNovo = 17000;
-        double pcUsado = 10000 ;
+        double pcUsado = 10000;
+
         if (valorAtual >= pcNovo) {
             System.out.println("Comprarei um pc novo!!");
         } else {
-            if (valorAtual <= pcUsado) ;
-            System.out.println("Comprarei um pc usado!");
+            if (valorAtual <= pcUsado) {
+                System.out.println("Comprarei um pc usado!");
+            }
         }
     }
 }
-
-
